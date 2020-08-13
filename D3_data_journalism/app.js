e392eb0b56b676896f50ac3c0f9712a8bdb4a447
Taniya Chhabra <chhabra.taniya@gmail.com>
2020-08-12 17:23:14 -0700

    updated tool tip

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -29,16 +29,18 @@ d3.csv("data.csv").then(function(Data) {
     Data.forEach(function(data) {
       data.age = +data.age;
       data.smokes = +data.smokes;
+      data.abbr = data.abbr;
     });
+
 
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(Data, d => d.age)])
+      .domain([30, d3.max(Data, d => d.age)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(Data, d => d.smokes)])
+      .domain([8, d3.max(Data, d => d.smokes)])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -63,16 +65,22 @@ d3.csv("data.csv").then(function(Data) {
     .append("circle")
     .attr("cx", d => xLinearScale(d.age))
     .attr("cy", d => yLinearScale(d.smokes))
-    .attr("r", "15")
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
+    .attr("r", "10")
+    .attr("fill", "#98ceeb")
+    // .attr("opacity", ".5");
 
-    // var text = groups.append("text")
-    // .data(sampleData)
-    // .text( function (d) { return "( " + d.cx + ", " + d.cy +" )"; })
-    // .attr("font-family", "sans-serif")
-    // .attr("font-size", "20px")
-    // .attr("fill", "red");
+    var text = chartGroup.selectAll()
+    .data(Data)
+    .enter()
+    .append("text")
+    .attr("dx", d => xLinearScale(d.age))
+    .attr("dy", d => yLinearScale(d.smokes))
+    .text(d => (d.abbr))
+    .attr("fill", "white")
+    .attr("font-size", 8)
+    .style("text-anchor", "middle");
+
+console.log(Data.abbr);
 
     // Create axes labels
     chartGroup.append("text")
@@ -81,12 +89,36 @@ d3.csv("data.csv").then(function(Data) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Smokes");
+      .text("Smokes (%)")
+      .attr("font-weight", 900)
+      .attr("font-size", 20);
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Age");
+      .text("Age(Median)")
+      .attr("font-weight", 900)
+      .attr("font-size", 20);
   }).catch(function(error) {
     console.log(error);
   });
+
+  var toolTip = d3.tip()
+  .attr("class", "tooltip")
+  .offset([80, -60])
+  .html(function(d) {
+    return (`${d.state}<br>Age (%): ${d.age}<br>Smokes(%): ${d.smokes}`);
+  });
+
+// Create tooltip in the chart
+// ==============================
+chartGroup.call(toolTip);
+
+chartGroup.on("click", function(data) {
+  toolTip.show(data, this);
+})
+  // onmouseout event
+  .on("mouseout", function(data, index) {
+    toolTip.hide(data);
+  });
+
